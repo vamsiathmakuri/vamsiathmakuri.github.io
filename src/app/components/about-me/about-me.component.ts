9536f7d4151317b881e0d8965e43c409da747ef4
Vamsi Athmakuri <athmakurivamshi@gmail.com>
2023-07-09 22:41:08 +0530

    Added About Me

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,12 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-about-me',
     templateUrl: 'about-me.component.html',
     styleUrls: ['about-me.component.scss']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, AfterViewInit {
+
+    @ViewChild('scrollview') elementRef?: ElementRef<HTMLDivElement>;
+
+    active: string = 'bio';
+
     constructor() { }
 
+    ngAfterViewInit(): void {
+        this.elementRef?.nativeElement.addEventListener('scroll', (e: any) => {
+            const childs = [...e.target.children].map((data) => {
+                return {
+                    id: (data as HTMLElement).id,
+                    height: (data as HTMLElement).offsetHeight
+                }
+            });
+            
+            const scrollHeight = e.target.scrollTop;
+
+            let position = 0;
+            let positionScroll = scrollHeight;
+
+            for(const { id, height } of childs) {
+                if(positionScroll > height) {
+                    positionScroll -= height;
+                    position++;
+                } else {
+                    if(height - positionScroll < height / 2) {
+                        position++;
+                    }
+                    break;
+                }
+            }
+
+            this.active = childs[position].id;
+        });
+    }
+
     ngOnInit() { }
+
+    goTo(htmlId: string) {
+        const element = document.getElementById(htmlId);
+        element?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
 }
--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -12,7 +12,28 @@ export class ContactMeComponent implements OnInit {
     message: string = '';
     date: Date = new Date();
 
+    submission: boolean = false;
+
     constructor() { }
 
     ngOnInit() { }
+
+    formUrl = () => `https://docs.google.com/forms/d/e/1FAIpQLSfKBoj0_62BfEP31zGu8ivUUPRS2pWMX64ZqMijERwz6X6MlA/formResponse?&submit=Submit?usp=pp_url&entry.1766847072=${encodeURI(this.name)}&entry.290273167=${encodeURI(this.email)}&entry.895993157=${encodeURI(this.message)}`;
+
+    submitResponse() {
+        if(this.name === '' || this.email === '' || this.message === '') return;
+
+        fetch(this.formUrl(), {
+            method: 'GET',
+        }).then((response) => {}).catch(() => {}).finally(() => {
+            this.submission = true;
+        });
+    }
+
+    clear() {
+        this.name = '';
+        this.email = '';
+        this.message = '';
+        this.submission = false;
+    }
 }
